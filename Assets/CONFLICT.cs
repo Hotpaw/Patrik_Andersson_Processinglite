8c5f19d85fd3b12cff440908281fafd9aa56b6e1
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-09-15 14:58:27 +0200

    skynet

---
+++ Assets/CONFLICT.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CONFLICT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // CREATE SKYNET
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
