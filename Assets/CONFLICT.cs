758e575d9c17d9e14c8f5995d0c582e437af778b
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-09-15 15:03:23 +0200

    update

--- Assets/CONFLICT.cs
+++ Assets/CONFLICT.cs
@@ -7,6 +7,8 @@ public class CONFLICT : MonoBehaviour
 
     int bobby = 1213;
 
+    GameObject lol;
+    List<Transform> trans = new List<Transform>();
     // Start is called before the first frame update
     void Start()
     {
